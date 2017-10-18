5332ee4b1f9f8b582aa027704602c32c8dee6675
Francisco Ko <francisco.ko86@gmail.com>
2017-10-17 18:29:54 -0700

    working google places with radius search

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,17 +2,26 @@ $(document).ready(function() {
 
     $("#submit-button").on("click", function() { 
 
+        var name = $("#name-input").val().trim();
         var address = $("#address-input").val().trim();
         
         if(!(address === null || address.match(/^ *$/) !== null)) {
-            //callGoogle(address.replace(' ', '+'), "");\
-            var testUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=803+archer+st+san+diego+ca&key=AIzaSyDFJA-1O_YEj46FAJKk48WibUoT7YHdK1E";
-            var options = proxyOptions('GET', testUrl);
-
-            request(options)
-            .then(function (response) {
-                console.log(coordinates(JSON.parse(response)));
+            var frmtAddr = addressSearch(address);
+            
+            request(proxyOptions('GET', frmtAddr)) 
+            .then(function (coordsResponse) {
+                var coords = coordinates(JSON.parse(coordsResponse));
+                return request(proxyOptions('GET', detailSearch(coords, name)));
+            })
+            .then(function (detailsResponse) {
+                console.log(JSON.parse(detailsResponse));
             });
         }
     });
 });
+
+function formatInput(field) {
+    return field.replace(' ', '+')
+                .replace('.', '+')
+                .replace(',', '+');
+}
