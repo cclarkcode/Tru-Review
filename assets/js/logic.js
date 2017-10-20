33faf27cd5b949629eebd086af326746588750ff
Francisco Ko <francisco.ko86@gmail.com>
2017-10-19 20:29:34 -0700

    more code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,10 +7,33 @@ $(document).ready(function() {
         if(!(address === null || address.match(/^ *$/) !== null)) {
             var frmtAddr = addressSearch(address);
             var frmtName = formatInput(name);
+            var test = "http://localhost:8080/";
+            // request({
+            //     method: 'POST',
+            //     url: test+yelpAuthUrl,
+            //     headers: {
+            //         "Access-Control-Allow-Headers": "*",
+            //         "origin": "*",
+            //         "grant_type": 'client_credentials',
+            //         "client_id": '800_1iRDDSx1mkxG680z6A',
+            //         "client_secret": 'ug0rVli4OnqYsHKgv8epYIBynvQZZlOmH3z3Luz5fddVfrXj4qE9Z8shxjlpRI7t'
+            //        }
+            // })  
+            // .then(function(tokenResponse) {
+            //     console.log(tokenResponse);
+                
+            // });
+            
+            request(
+                proxyOptions('POST', buildProxyUrl(yelpAuthTokenUrl)
+            ))
+            .then(function(tokenResponse) {
+                console.log(tokenResponse);
+                
+            });
 
             request(proxyOptions('GET', frmtAddr))  
             .then(function (coordsResponse) {
-
                 var coords = coordinates(coordsResponse);
                 return request({
                     method: 'POST',
@@ -21,11 +44,10 @@ $(document).ready(function() {
                 });
             })
             .then(function (detailsResponse) {
-                console.log(JSON.parse(detailsResponse));
+                var zomatoPlace = JSON.parse(detailsResponse).restaurants[0];
+                console.log(zomatoPlace.restaurant.name);
+                console.log("Zomato: " + formatReview(zomatoPlace.restaurant.user_rating.aggregate_rating));
             });
-            // .then(function(restraurantResponse) {
-            //     console.log(restraurantResponse);
-            // });
 
             request(proxyOptions('GET', frmtAddr))  
             .then(function (coordsResponse) {
@@ -36,7 +58,8 @@ $(document).ready(function() {
                 return getRestaurant(name, detailsResponse);
             })
             .then(function(restraurantResponse) {
-                console.log(restraurantResponse);
+                console.log(restraurantResponse.name);
+                console.log("Google: " + formatReview(restraurantResponse.rating));
             });
         }
     });
@@ -46,4 +69,8 @@ function formatInput(field) {
     return field.replace(' ', '+')
                 .replace('.', '+')
                 .replace(',', '+');
+}
+
+function formatReview(review) {
+    return review === "0" ? "Tu Fue Reviews" : review;
 }
