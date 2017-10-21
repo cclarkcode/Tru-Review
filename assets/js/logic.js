cc5ba28c2508d20d3c14fb5eab28540d0fbb3ac1
Francisco Ko <francisco.ko86@gmail.com>
2017-10-21 09:02:25 -0700

    fill in ui code

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,18 +1,22 @@
 var avgRating = 0;
 var ratingCount = 0;
+var frmtName;
+var frmtAddr;
 
 $(document).ready(function() {
+    $("#name-submit").on("click", function() { 
+        var name = $("#name-search").val().trim();
+        if(!(name === null || name.match(/^ *$/) !== null)) {
+            frmtName = formatInput(name);
+        }
+    });
 
-    $("#submit-button").on("click", function() { 
-        var name = $("#name-input").val().trim();
-        var address = $("#address-input").val().trim();
-        
+    $("#address-submit").on("click", function() {
         if(!(address === null || address.match(/^ *$/) !== null)) {
-            var frmtAddr = addressSearch(address);
-            var frmtName = formatInput(name);
+            var address = $("#address-input").val().trim();
+            frmtAddr = addressSearch(address);
 
             var yelpAuthToken;
-            var ratings = [];
 
             request(proxyOptions('POST', yelpAuthTokenUrl))
             .then(function(tokenResponse) {
@@ -40,7 +44,7 @@ $(document).ready(function() {
                         addRatingScore(rating);
                     }
 
-                    //set rating element here
+                    // UI/DB LOGIC HERE
                    
                     console.log("Yelp Rating: " + formatReview(yelpPlace.rating));
                 });
@@ -61,7 +65,7 @@ $(document).ready(function() {
                         addRatingScore(rating);
                     }
 
-                    //set rating element here
+                    // UI/DB LOGIC HERE
 
                     console.log("Zomato Rating: " + formatReview(rating));
                 });
@@ -78,7 +82,7 @@ $(document).ready(function() {
                         addRatingScore(rating);
                     }
 
-                    //set rating element here
+                    // UI/DB LOGIC HERE
                     
                     console.log("Google Rating: " + formatReview(rating));
                 });
@@ -97,80 +101,8 @@ function formatReview(review) {
     return review === "0" ? "Tu Fue Reviews" : review;
 }
 
-function getGoogleRating(address, name) {
-    return new Promise((resolve, reject) => {
-        request(proxyOptions('GET', address))  
-        .then(function (coordsResponse) {
-            var coords = coordinates(coordsResponse);
-            return request(proxyOptions('GET', googleDetailSearch(coords, name)));
-        })
-        .then(function (detailsResponse) {
-            return getRestaurant(name, detailsResponse);
-        })
-        .then(function(restraurantResponse) {
-            //restraurantResponse.rating > 0 ? review += 1 : review;
-            console.log("Google Rating: " + formatReview(restraurantResponse.rating));
-            resolve(formatReview(restraurantResponse.rating));
-        });
-    });
-}
-
 function addRatingScore(rating) {
     avgRating += rating;
     ratingCount++;
-    console.log(avgRating/ratingCount);
+    //DISPLAY AND UPDATE AVERAGE RATING
 }
-
-// request(proxyOptions('POST', yelpAuthTokenUrl))
-// .then(function(tokenResponse) {
-//     yelpAuthToken = JSON.parse(tokenResponse).access_token;
-// })
-// .then(function(){
-//     return request(proxyOptions('GET', frmtAddr));
-// })
-// .then(function(coordsResponse){
-//     var coords = coordinates(coordsResponse);
-//     return request({
-//         method: 'GET',
-//         url: buildProxyUrl(yelpRestaurantSearch(coords, frmtName)),
-//         headers: {
-//             "authorization": "Bearer " + yelpAuthToken
-//            }
-//     });
-// })
-// .then(function(yelpResponse) {
-//     var yelpPlace = JSON.parse(yelpResponse).businesses[0];
-//     yelpPlace.rating > 0 ? review += 1 : review;
-//     console.log("Yelp Rating: " + formatReview(yelpPlace.rating));
-// });
-
-// request(proxyOptions('GET', frmtAddr))  
-// .then(function (coordsResponse) {
-//     var coords = coordinates(coordsResponse);
-//     return request({
-//         method: 'POST',
-//         url: zomatoRestaurantSearch(coords, frmtName),
-//         headers: {
-//             "user-key": "f73f1e3b1f28a94ae801eb97cf84f822"
-//            }
-//     });
-// })
-// .then(function (detailsResponse) {
-//     var zomatoPlace = JSON.parse(detailsResponse).restaurants[0];
-//     zomatoPlace.restaurant.user_rating.aggregate_rating > 0 ? review += 1 : review;
-//     console.log("Zomato Rating: " + formatReview(zomatoPlace.restaurant.user_rating.aggregate_rating));
-// });
-
-// request(proxyOptions('GET', frmtAddr))  
-// .then(function (coordsResponse) {
-//     var coords = coordinates(coordsResponse);
-//     return request(proxyOptions('GET', googleDetailSearch(coords, frmtName)));
-// })
-// .then(function (detailsResponse) {
-//     return getRestaurant(name, detailsResponse);
-// })
-// .then(function(restraurantResponse) {
-//     //restraurantResponse.rating > 0 ? review += 1 : review;
-//     console.log("Google Rating: " + formatReview(restraurantResponse.rating));
-//     return formatReview(restraurantResponse.rating);
-// });
